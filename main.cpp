6a5c7a0b6da14c32c13cd241b46297745388ff1d
Antarecke <2395326872@qq.com>
2024-09-26 16:43:42 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -4,4 +4,21 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    int temp = b;
+    if (a < b) {
+      b = a;
+      a = temp;
+    }
+    temp = c;
+    if (temp > b) {
+      c = b;
+      b = temp;
+    }
+    if (temp > a) {
+      b = a;
+      a = temp;
+    }
+    result.max = a;
+    result.mid = b;
+    result.min = c;
 }
